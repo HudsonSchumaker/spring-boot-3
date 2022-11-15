5bd6aa2d4c10bae0f2cefca27741b46b34455f71
HudsonSchumaker <hudson.schumaker@me.com>
2022-11-15 22:53:46 +0100

    Order Service

---
+++ payment/src/main/java/br/com/schumaker/payment/PaymentApplication.java
@@ -0,0 +1,17 @@
+package br.com.schumaker.payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableEurekaClient
+@EnableFeignClients
+@SpringBootApplication
+public class PaymentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentApplication.class, args);
+    }
+
+}
