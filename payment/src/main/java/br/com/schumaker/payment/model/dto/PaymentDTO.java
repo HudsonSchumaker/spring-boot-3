8d01575b82658b3435889c43483c8f185225a979
HudsonSchumaker <hudson.schumaker@me.com>
2022-12-03 00:04:19 +0100

    PaymentDTO

--- payment/src/main/java/br/com/schumaker/payment/model/dto/PaymentDTO.java
+++ payment/src/main/java/br/com/schumaker/payment/model/dto/PaymentDTO.java
@@ -1,16 +1,20 @@
 package br.com.schumaker.payment.model.dto;
 
 import br.com.schumaker.payment.model.Status;
-
+import lombok.Getter;
+import lombok.Setter;
 import java.math.BigDecimal;
 
-public record PaymentDTO(Long id,
-                         BigDecimal value,
-                         String name,
-                         String number,
-                         String expiration,
-                         String code,
-                         Status status,
-                         Long orderId,
-                         Long paymentMethod) {
+@Getter
+@Setter
+public class PaymentDTO {
+    private Long id;
+    private BigDecimal value;
+    private String name;
+    private String number;
+    private String expiration;
+    private String code;
+    private Status status;
+    private Long orderId;
+    private Long paymentMethod;
 }
