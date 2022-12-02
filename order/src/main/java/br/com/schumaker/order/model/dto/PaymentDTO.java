a3d6eacf39f9c9dd6be2ecedbd04629dde2f4c92
HudsonSchumaker <hudson.schumaker@me.com>
2022-12-03 00:04:06 +0100

    Payment

---
+++ order/src/main/java/br/com/schumaker/order/model/dto/PaymentDTO.java
@@ -0,0 +1,23 @@
+package br.com.schumaker.order.model.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@ToString
+public class PaymentDTO {
+    private Long id;
+    private BigDecimal value;
+    private String name;
+    private String number;
+    private String expiration;
+    private String code;
+    private String status;
+    private Long orderId;
+    private Long paymentMethod;
+    private String log;
+}
