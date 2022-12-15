f9d3db35db4be8808203857e82cb423f753806ed
HudsonSchumaker <hudson.schumaker@me.com>
2022-12-15 14:54:54 +0100

    Payment Application

--- finance/src/main/java/br/com/schumaker/finance/service/PaymentsListener.java
+++ finance/src/main/java/br/com/schumaker/finance/service/PaymentsListener.java
@@ -2,13 +2,20 @@ package br.com.schumaker.finance.service;
 
 import br.com.schumaker.finance.model.dto.PaymentDTO;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 @Component
 public class PaymentsListener {
 
-    @RabbitListener(queues = "payment.done")
-    public void getMessage(PaymentDTO message) {
+    @RabbitListener(queues = "finance.order")
+    public void getMessage(@Payload PaymentDTO message) {
+        System.out.println(message.getId());
+        System.out.println(message.getNumber());
+
+        if (message.getNumber().equals("000")) {
+            throw new RuntimeException("simulating an error");
+        }
         System.out.println(message.toString());
     }
 }
