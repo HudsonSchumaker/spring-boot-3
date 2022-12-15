221e4898a97280fe7745ba2b0be503f522b1760f
HudsonSchumaker <hudson.schumaker@me.com>
2022-12-15 14:55:07 +0100

    Order Application.

--- order/src/main/java/br/com/schumaker/order/config/mq/OrderMqConfig.java
+++ order/src/main/java/br/com/schumaker/order/config/mq/OrderMqConfig.java
@@ -1,8 +1,12 @@
 package br.com.schumaker.order.config.mq;
 
+import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -21,5 +25,36 @@ public class OrderMqConfig {
         RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
         rabbitTemplate.setMessageConverter(messageConverter);
         return rabbitTemplate;
+    }
+
+    @Bean
+    public Queue paymentQueue() {
+        return QueueBuilder
+                .nonDurable("payment.order")
+                .build();
+    }
+
+    @Bean
+    public FanoutExchange fanoutExchange() {
+        return ExchangeBuilder
+                .fanoutExchange("payment.ex")
+                .build();
+    }
+
+    @Bean
+    public Binding bindOrderPayment(FanoutExchange fanoutExchange) {
+        return BindingBuilder
+                .bind(paymentQueue())
+                .to(fanoutExchange());
+    }
+
+    @Bean
+    public RabbitAdmin createRabbitAdmin(ConnectionFactory conn) {
+        return new RabbitAdmin(conn);
+    }
+
+    @Bean
+    public ApplicationListener<ApplicationReadyEvent> initAdmin(RabbitAdmin rabbitAdmin) {
+        return event -> rabbitAdmin.initialize();
     }
 }
